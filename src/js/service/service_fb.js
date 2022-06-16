98ffb6c576d0994e133762a14dcdebcd6041f612
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-16 20:27:07 +0300

    add firebase package and imports

---
+++ src/js/service/service_fb.js
@@ -0,0 +1,5 @@
+import { initializeApp } from 'firebase/app';
+
+import { firebaseConfig } from '../settings/fb_config';
+
+const app = initializeApp(firebaseConfig);
---
+++ src/js/settings/fb_config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDv7KgyEiqgjpoW-k5Ei81HkAGFskZT4v8',
+  authDomain: 'filmoteka-bc18.firebaseapp.com',
+  projectId: 'filmoteka-bc18',
+  storageBucket: 'filmoteka-bc18.appspot.com',
+  messagingSenderId: '424062245497',
+  appId: '1:424062245497:web:4c44a4b0aee105be49d2a8',
+};
