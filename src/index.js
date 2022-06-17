044bcf4ca3bf01605f12e0c65ee3b3e35070602e
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-17 19:35:51 +0300

    fis import refs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { moviePagination } from './js/pagination';
 import { closeModalAuth } from './js/modal-auth';
 import { onFormSubmit } from './js/authForm';
 import { updateForm } from './js/authForm';
+import refs from './js/refs';
 //
 
 refs.form.addEventListener('submit', onFormSubmit);
