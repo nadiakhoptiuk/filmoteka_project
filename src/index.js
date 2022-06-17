0a24a19cef5096a798bb18f0cc73818213cf87fc
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-17 22:36:02 +0300

    add authorization with google account and update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { markupMoviesGallery } from './js/template';
-import { pagination } from './js/pagination';
-import { moviePagination } from './js/pagination';
+import { pagination, moviePagination } from './js/pagination';
 import { closeModalAuth } from './js/modal-auth';
-import { onFormSubmit } from './js/authForm';
-import { updateForm } from './js/authForm';
+import { onFormSubmit, updateForm } from './js/authForm';
+import { userSignInWithGoogle } from './js/service/service_fb';
 import refs from './js/refs';
 //
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.formSwitchBtn.addEventListener('click', updateForm);
 refs.modalAuthEl.addEventListener('click', closeModalAuth);
+refs.formSignInWithGoogle.addEventListener('click', userSignInWithGoogle);
 //
 document.addEventListener('DOMContentLoaded', markupMoviesGallery);
 pagination.on('afterMove', moviePagination);
--- src/js/service/service_fb.js
+++ src/js/service/service_fb.js
@@ -4,12 +4,13 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
+  signInWithPopup,
 } from 'firebase/auth';
 import { firebaseConfig } from '../settings/fb_config';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
 function userRegistration(email, password) {
   createUserWithEmailAndPassword(auth, email, password)
@@ -37,4 +38,30 @@ function userSignIn(email, password) {
     });
 }
 
-export { userRegistration, userSignIn };
+function userSignInWithGoogle(evt) {
+  signInWithPopup(auth, provider)
+    .then(result => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(evt.target);
+
+      console.log(user);
+
+      // ...
+    })
+    .catch(error => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+export { userRegistration, userSignIn, userSignInWithGoogle };
