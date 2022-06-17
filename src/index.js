2b4bef376b7e481f2fcc0cfc636276f3cf2ffe99
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-17 19:30:50 +0300

    fix imports at index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import { markupMoviesGallery } from "./js/template";
+import { markupMoviesGallery } from './js/template';
 import { pagination } from './js/pagination';
-import { moviePagination } from './js/pagination'
+import { moviePagination } from './js/pagination';
+import { closeModalAuth } from './js/modal-auth';
+import { onFormSubmit } from './js/authForm';
+import { updateForm } from './js/authForm';
 //
 
 refs.form.addEventListener('submit', onFormSubmit);
