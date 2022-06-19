085f38e6c13558a71b000deab749dc0d1025714f
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-06-19 12:27:22 +0300

    fix-css-style

--- src/js/created-modal-film.js
+++ src/js/created-modal-film.js
@@ -1,11 +1,10 @@
+import refs from "./refs";
 export function createModalFilm(ev) {
     const object = ev[0]
       const { original_title, title, vote_average, vote_count, poster_path, overview, popularity, allGenres
     } = object;
+    const htmlImg= `<img src="https://image.tmdb.org/t/p/original${poster_path}" alt="poster ${title}" class="modal-film__img">`
     const htmlPoint = `
-     
-    <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="poster ${title}" class="modal-film__img">
-    <div>
             <h2 class="modal-film__name">${title}</h2>
             <div class="modal-film__rating">
                 <div class="modal-film__list-name">
@@ -23,8 +22,9 @@ export function createModalFilm(ev) {
             </div>
             <h3 class="modal-film__title">ABOUT</h3>
             <p class="modal-film__description">${overview}</p>
-        </div>
        `
-    const modalFilm = document.querySelector(".modal-film--add");
-    modalFilm.innerHTML = htmlPoint;
+    const img = refs.modalFilmImg;
+    const point = refs.modalFilmHtml
+img.innerHTML = htmlImg;
+   point.innerHTML = htmlPoint;
  }
