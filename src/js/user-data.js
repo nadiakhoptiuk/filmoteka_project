b0e048c09577138076d1856f05b552d2b2612b8e
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-06-20 10:53:13 +0300

    fix user-data export

--- src/js/user-data.js
+++ src/js/user-data.js
@@ -44,7 +44,7 @@ function onAddToQueueBtnClick(evt) {
   if (isUserSignIn(userId) && btnTitle.trim() === 'add to queue') {
     addMovieToQueue(data);
   }
-  
+
   if (isUserSignIn(userId) && btnTitle.trim() === 'remove from queue') {
     removeMovieFromQueue(data);
   }
