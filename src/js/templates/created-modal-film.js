8f4c0196dd7910785803033f7fa6e33987d3142a
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-06-22 15:25:01 +0300

    1

--- src/js/templates/created-modal-film.js
+++ src/js/templates/created-modal-film.js
@@ -1,4 +1,5 @@
 import { modalFilmImg, modalFilmHtml } from '../refs/refs';
+import { audit } from '../service/audit';
 
 export function createModalFilm(ev) {
   const {
@@ -9,23 +10,23 @@ export function createModalFilm(ev) {
     poster_path,
     overview,
     popularity,
-    allGenres,
+    genres,
   } = ev;
   const htmlImg = `<img src="https://image.tmdb.org/t/p/w500${poster_path === null ? '/h5oGodvcoq8cyIDTy79yKn4qbey.jpg' : poster_path}" alt="poster ${title}" class="modal-film__img">`;
   const htmlPoint = `
-            <h2 class="modal-film__name">${title}</h2>
+            <h2 class="modal-film__name">${audit(title)}</h2>
             <div class="modal-film__rating">
                 <div class="modal-film__list-name">
-                    <p class="modal-film__item-name">Vote / Votes</p>
-                    <p class="modal-film__item-name">Popularity</p>
-                    <p class="modal-film__item-name">Original Title</p>
-                    <p class="modal-film__item-name">Genre</p>
+                    <p class="modal-film__item-name vote">Vote / Votes</p>
+                    <p class="modal-film__item-name popularity">Popularity</p>
+                    <p class="modal-film__item-name origin">Original Title</p>
+                    <p class="modal-film__item-name genre">Genre</p>
                 </div>
                 <div class="modal-film__list-description">
-                    <p class="modal-film__item-description"><span class="modal-film--style">${vote_average}</span> / ${vote_count}</p>
-                    <p class="modal-film__item-description">${popularity}</p>
-                    <p class="modal-film__item-description">${original_title}</p>
-                    <p class="modal-film__item-description">${allGenres}</p >
+                    <p class="modal-film__item-description vote"><span class="modal-film--style">${audit(vote_average)}</span> / ${audit(vote_count)}</p>
+                    <p class="modal-film__item-description popularity">${audit(popularity)}</p>
+                    <p class="modal-film__item-description origin">${audit(original_title)}</p>
+                    <p class="modal-film__item-description genre">${genres.flatMap(ev=> ev.name).join(", ")}</p >
                 </div>
             </div>
             <h3 class="modal-film__title">ABOUT</h3>
