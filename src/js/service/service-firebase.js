56895126d7a8def73810cabb540065fe603acc9b
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-22 11:54:13 +0300

    fix fixes

--- src/js/service/service-firebase.js
+++ src/js/service/service-firebase.js
@@ -24,7 +24,7 @@ import {
   getWatchedFilms,
 } from '../templates/render-gallery-my-library';
 import { getUserIdAfterSignIn } from '../modals/auth-form';
-import { signOutBtn, signOutWrap, form, body } from '../refs/refs';
+import { signOutBtn, signOutWrap, form, homePage } from '../refs/refs';
 import { filterFilmByBtn } from '../modals/modal-film';
 import { getDataFromFirebase } from '../utils/get-data-from-fb';
 
@@ -55,7 +55,7 @@ function userSignIn(email, password) {
 
       signOutBtnShow();
       closeModalAuth();
-      if (body.classList.contains('modal-film-is-open') !== true) {
+      if (homePage.classList.contains('modal-film-is-open') !== true) {
         getWatchedFilms(user.uid);
       }
 
@@ -87,7 +87,7 @@ function userSignInWithGoogle(evt) {
       closeModalAuth();
       getDataFromFirebase(user.uid);
 
-      if (body.classList.contains('modal-film-is-open') !== true) {
+      if (homePage.classList.contains('modal-film-is-open') !== true) {
         getWatchedFilms(user.uid);
       }
       filterFilmByBtn(user.uid);
